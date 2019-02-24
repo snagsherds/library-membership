d62f16f79c81d2d854a8bb7b1df0d29071b8e779
Rohan Sarkar <rohan.manab@gmail.com>
2019-02-25 03:21:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDxcCDtXlXNQmyXrEI_J6V6buW8jTEhEA4",
+    authDomain: "library-app-32c77.firebaseapp.com",
+    databaseURL: "https://library-app-32c77.firebaseio.com",
+    projectId: "library-app-32c77",
+    storageBucket: "library-app-32c77.appspot.com",
+    messagingSenderId: "366884373368"
+  };
+  firebase.initializeApp(config);
